be17357badbb15162b43f3116d210a27324bafdb
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-26 11:47:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-
-import React from "react";
+import React, { useState } from "react";
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-       <h1>Parent Component</h1>
-      <div></div>
-    <p>Selected Option:</p>
-    </div>
-  )
-}
+  const [selectedOption, setSelectedOption] = useState(null);
 
-export default App
+  // Function to update the selectedOption state
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <p>Selected Option: {selectedOption}</p>
+      <ChildComponent1 onOptionChange={handleOptionChange} />
+      <ChildComponent2 onOptionChange={handleOptionChange} />
+    </div>
+  );
+};
+
+export default App;
